4683abc5744459370cf0290a9b382267990b7801
thenufreshdesk <thenmozhe.d@freshdesk.com>
2015-03-28 17:39:42 +0530

    add

--- config/production.js
+++ config/production.js
@@ -1,10 +1,26 @@
+// See `parse_url` above
+var MONGO_PARSED = parse_url(process.env.MONGOHQ_URL);
+
 var config = {
   detailedErrors: false
-, hostname: null
-, port: process.env.PORT
+, debug: false
+, hostname: "0.0.0.0"
+, port: process.env.PORT || 4000
+, model: {
+    defaultAdapter: 'mongo'
+  }
+, db: {
+    mongo: {
+      username: MONGO_PARSED.user
+    , dbname: MONGO_PARSED.path.substring(1)    // Get rid of the leading `/`
+    , password: MONGO_PARSED.pass
+    , host: MONGO_PARSED.host
+    , port: parseInt(MONGO_PARSED.port)
+    }
+  }
 , sessions: {
-    store: 'memory'
-  , key: 'sid'
+    store: 'cookie'
+  , key: 'did'
   , expiry: 14 * 24 * 60 * 60
   }
 };
